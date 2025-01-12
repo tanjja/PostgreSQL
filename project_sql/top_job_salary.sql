0a346e8327099d3600c80b63ffee94373c53d032
Justin Tan <justin.tan56@yahoo.com>
2025-01-12 21:54:43 +0800

    query1

---
+++ project_sql/top_job_salary.sql
@@ -0,0 +1,32 @@
+/*
+Objective: Find which data and business analyst jobs have the highest salaries
+
+Procedure:
+- Filter job titles which include the words "business", "data", and "analyst"
+- Remove jobs which have no salary (nulls)
+- Order by salary per year average
+
+Additional:
+- Filter jobs available in the Philippines or remotely (Smaller dataset)
+*/
+
+SELECT
+    job_id,
+    job_title,
+    name AS company_name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+LEFT JOIN 
+    company_dim
+    ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short IN ('Data Analyst','Business Analyst')
+    AND job_location IN ('Philippines', 'Anywhere')
+    AND salary_year_avg IS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 10;
